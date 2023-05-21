51bdfc74fafad401e1b0c4fc881073c9abcaaf81
DavidRR-F <david.rosefranklin96@gmail.com>
2023-05-21 09:56:50 -0400

    add user token endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,17 @@
 from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
 
 class CreateUserView(generics.CreateAPIView):
+    """Create New User in System"""
     serializer_class = UserSerializer
+    
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    rederer_classes = api_settings.DEFAULT_RENDERER_CLASSES
